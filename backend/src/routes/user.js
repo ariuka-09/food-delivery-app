7c9d845e8e1e0c149303589effedcd417a9b2ed9
ariuka-09 <ariuntuguldur3@gmail.com>
2025-10-29 09:48:41 +0800

    finalized user CRUD

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUsers, logIn, signUp } from "../controller/user.js";
+import { getUsers, logIn, signUp, updateUser } from "../controller/user.js";
 
 export const userRouter = Router();
 userRouter
 .post('/signUp', signUp)
 .get('/', getUsers)
 .post('/logIn',logIn )
+.patch('/:id', updateUser)
