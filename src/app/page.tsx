c9437578b6aced1bf9ca56213bccfda8a47b1ac6
ariuntuguldur3@gmail.com <ariuntuguldur3@gmail.com>
2025-10-30 00:03:21 +0800

    placed the start of homepage, made it possible to map out the food and food category data to frontend, found a way to make my backend and frontend concurrently

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,39 @@
+import { Food, FoodCategory } from "@/types";
+import axios from "axios";
 import Image from "next/image";
 
-export default function Home() {
-  return ();
+export default async function Home() {
+  // const foodCategories = await fetch("http://localhost:5000/foodCategory");
+  // const data = await foodCategories.json();
+  // console.log(data);
+
+  const foodCategories = await axios.get<FoodCategory[]>(
+    "http://localhost:5000/foodCategory"
+  );
+
+  const foods = await axios.get<Food[]>("http://localhost:5000/food");
+  console.log("food data", foods.data);
+
+  return (
+    <div>
+      <div>
+        {foodCategories.data.map((categories) => {
+          const { _id, categoryName } = categories;
+          return (
+            <div>
+              <div>{categoryName}</div>
+              <div>
+                <p className="font-bold">
+                  {foods.data.map((food) => {
+                    const { foodName, category } = food;
+                    if (_id == category) return foodName;
+                  })}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
